98aa1c95b35aa30b8fc7de61d262a06f9ffb0561
Sucipto <show.cheap@gmail.com>
2016-10-26 17:11:41 +0700

    Add SQLite Config

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QDebug>
+#include <QSqlQuery>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -7,7 +9,20 @@ MainWindow::MainWindow(QWidget *parent) :
 {
     ui->setupUi(this);
 
+    // Init Database
+    database = QSqlDatabase::addDatabase("QSQLITE");
+    database.setDatabaseName("KBBI.db");
+
+    qDebug()<< "Is connect? " << database.open();
+
+    QSqlQuery testquery;
+    if(testquery.exec("SELECT COUNT(*) FROM test")){
+        qDebug()<< "Query OK ";
+    }else{
+        qDebug()<< "Query Not OK ";
+    }
 }
+
 
 MainWindow::~MainWindow()
 {
