3aed77620e029328a1be710b8554fd16a55216e2
Christian Kurniawan Ginting <saa7.go@gmail.com>
2016-11-01 21:42:44 +0700

    ubah dan hapus kode yang tidak diperlukan

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -134,14 +134,14 @@ void MainWindow::searchQuery(QString keyword)
 
 void MainWindow::modifyHtmlTag(QString &text)
 {
-    text.replace("&lt;", "<").replace("&gt;", ">");
+//    text.replace("&lt;", "<").replace("&gt;", ">");
     text.replace(0, QString("<b>").length(), "<b style='color:red;background-color:yellow'>");
     text.replace("<b>", "<b style='color:red'>");
     text.replace("<i>n</i>", "<i style='color:blue'>n</i>");
     text.replace("<i>v</i>", "<i style='color:blue'>v</i>");
     text.replace("<i>", "<i style='color:green'>");
     text.replace("<br>", "<br/><br/>");
-    text.replace("<", "&lt;").replace(">","&gt;");
+//    text.replace("<", "&lt;").replace(">","&gt;");
 }
 
 void MainWindow::slotCariKata()
@@ -154,12 +154,14 @@ void MainWindow::pilihKata(QModelIndex index)
 {
 
     QString resultText = kamusModel->data(index.sibling(index.row(),1)).toString();
-    modifyHtmlTag(resultText);
 
     QTextDocument doc;
     doc.setHtml(resultText);
 
-    ui->detailResult->setText(doc.toPlainText());
+    QString modifiedText = doc.toPlainText();
+    modifyHtmlTag(modifiedText);
+
+    ui->detailResult->setText(modifiedText);
 
 }
 
