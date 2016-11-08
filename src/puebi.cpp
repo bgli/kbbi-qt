1cbde11eba4d536043a7edc795fc30508e957216
rania el_amina <reaamina@gmail.com>
2016-11-08 16:58:13 +0700

    pembenahan puebi.cpp dan penambahan instalsi q55webkit pada .travis.yml

--- src/puebi.cpp
+++ src/puebi.cpp
@@ -1,11 +1,13 @@
 #include "puebi.h"
 #include "ui_puebi.h"
+#include "QtWebKit/QWebView"
 
 PUEBI::PUEBI(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::PUEBI)
 {
     ui->setupUi(this);
+    ui->webView->setUrl(QUrl("file:///usr/share/KBBI-Qt/data/puebi.html"));
 }
 
 PUEBI::~PUEBI()
